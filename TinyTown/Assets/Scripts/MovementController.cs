aa244d9da06322535d288c615e785768d4818a35
mws75 <mwspencer75@gmail.com>
2020-01-06 21:28:37 -0800

    Created the Cat-Idel-Back

---
+++ TinyTown/Assets/Scripts/MovementController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    //properties
+    public float movementSpeed = 3.0f;
+
+    Vector2 movement = new Vector2();
+
+    Rigidbody2D rigidbody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        movement.Normalize();
+
+        rigidbody.velocity = movement * movementSpeed;
+    }
+}
